3af9eb4359115fd59d90a1ff551caf1d4df35011
banysk <banysk@users.noreply.github.com>
2020-10-16 01:17:14 +1000

    Initial commit

--- bossofthisgym/settings.py
+++ bossofthisgym/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['bossofthisgym.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
